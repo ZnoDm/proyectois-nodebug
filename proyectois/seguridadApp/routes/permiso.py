ff5a6c110d7d181c9582efcb78b5391476112a36
Cristo Cabrera <cfcabrerat@hotmail.com>
2022-08-12 21:35:04 -0500

    Cristo: Se habían duplicado varias cosas, este sí ta bien pipipi

--- proyectois/seguridadApp/routes/permiso.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from seguridadApp.views import listarpermiso
-
-urlpatterns = [
-    path('',listarpermiso,name="listarpermiso"),  
-]
--- proyectois/seguridadApp/routes/role.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from seguridadApp.views import listarrole
-
-urlpatterns = [
-    path('',listarrole,name="listarrole"),  
-]
