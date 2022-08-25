ffa200ff38072b38c4fc0bb734994441ace8fcec
Alejandro Angeles <n.angelesp@hotmail.com>
2022-08-25 02:03:52 -0500

    Dashboard termiando

--- ventasApp/views/api.py
+++ ventasApp/views/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from ventasApp.models import * 
 
 def get_countVentas(request,*args,**kwargs):
-    cantidad = PedidoVenta.objects.count()
+    cantidad = PedidoVenta.objects.filter(eliminado=False).count()
     data={
         'tipo':'ventas',
         'cantidad': cantidad
@@ -11,7 +11,7 @@ def get_countVentas(request,*args,**kwargs):
     return JsonResponse(data)
 
 def get_countCompras(request,*args,**kwargs):
-    cantidad = OrdenCompra.objects.count()
+    cantidad = OrdenCompra.objects.filter(eliminado=False).count()
     data={
         'tipo':'compras',
         'cantidad':cantidad
@@ -19,23 +19,39 @@ def get_countCompras(request,*args,**kwargs):
     return JsonResponse(data)
 
 def get_dataLine(request,*args,**kwargs):
+    list_trabajadores = []
+    list_ventasxtrabajador = []
+    trabajadores = Trabajador.objects.all().filter(eliminado=False).order_by('-idTrabajador').values()
+    for t in trabajadores:
+        trabajador = Trabajador.objects.get(idTrabajador=t['idTrabajador'])
+        cuenta = PedidoVenta.objects.filter(trabajador=trabajador).filter(eliminado=False).count()
+        list_trabajadores.append(str(t['apellidos'])+str(' ')+str(t['nombres']))
+        list_ventasxtrabajador.append(cuenta)
     data = {
-          'labels': ['January','February','March','April','May','June'],
+          'labels': list_trabajadores,
           'datasets': [{
-            'label': 'My First dataset',
+            'label': 'Cantidad de Ventas',
             'backgroundColor': 'rgb(255, 99, 132)',
             'borderColor': 'rgb(255, 99, 132)',
-            'data': [0, 10, 5, 2, 20, 30, 45],
+            'data': list_ventasxtrabajador,
           }]
     };
     return JsonResponse(data)
 
 def get_dataDona(request,*args,**kwargs):
+    list_productos = []
+    list_cantidadvendidos = []
+    productos = Producto.objects.all().filter(eliminado=False).order_by('-idProducto').values()
+    for t in productos:
+        producto = Producto.objects.get(idProducto=t['idProducto'])
+        cuenta = DetallePedidoVenta.objects.filter(producto=producto).filter(eliminado=False).count()
+        list_productos.append(str(t['nombre']))
+        list_cantidadvendidos.append(cuenta)
     data = {
-            'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            'labels': list_productos,
             'datasets': [{
                 'label': '# of Votes',
-                'data': [12, 19, 3, 5, 2, 3],
+                'data': list_cantidadvendidos,
                 'backgroundColor': [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
